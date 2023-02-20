b38b177b15638090c0866e8f1cdb25280da1a4eb
Felix Belz <st155595@stud.uni-stuttgart.de>
2023-02-20 16:08:43 +0100

    Added dummy structures to example applications

--- components/bpmn-q-vue/src/App.vue
+++ components/bpmn-q-vue/src/App.vue
@@ -3,11 +3,26 @@ import QFlowModeler from './components/QFlowModeler.vue';
 </script>
 
 <template>
-    <div style="height: 100%; width: 100%">
-        <QFlowModeler/>
+    <div style="display: flex; height: 100%">
+        <div style="width: 15%;"></div>
+        <hr/>
+        <div style="display: flex; flex-direction: column; height: 100%; width: 85%; flex: 1">
+            <h1 style="margin: 10px">Vue BPMN Modeler</h1>
+            <div id="button-container" style="display: flex; flex-shrink: 0; height: 60px; background-color: lightgrey">
+                <button class="my-button">Dummy</button>
+                <button class="my-button">Save</button>
+                <button class="my-button">Load</button>
+            </div>
+            <QFlowModeler/>
+            <div style="height: 80px; background-color: lightgrey"></div>
+        </div>
     </div>
 </template>
 
 <style scoped>
-
+    .my-button {
+        font-size: 18px;
+        width: 150px;
+        margin: 10px;
+    }
 </style>
