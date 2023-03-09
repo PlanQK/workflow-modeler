2281dd699e99191061ed2bb623e7371032678dcd
Felix Belz <st155595@stud.uni-stuttgart.de>
2023-03-09 23:40:54 +0100

    Cleaned up the deployment

--- components/bpmn-q/modeler-component/extensions/quantme/deployment/workflow/index.js
+++ components/bpmn-q/modeler-component/extensions/quantme/deployment/workflow/index.js
@@ -43,11 +43,6 @@ export default async function deployWorkflow(workflowName, workflowXml, viewMap)
   // add diagram to the body
   const bpmnFile = new File([workflowXml], fileName, {type: 'text/xml'});
   form.append('data', bpmnFile);
-  // const blob = new Blob([workflowXml], { type: 'text/xml' });
-  // form.append('file', blob, {
-  //   filename: fileName,
-  //   contentType: 'text/xml'
-  // });
 
   // upload all provided views
   for (const [key, value] of Object.entries(viewMap)) {
@@ -65,11 +60,6 @@ export default async function deployWorkflow(workflowName, workflowXml, viewMap)
     const response = await fetch(config.getCamundaEndpoint() + '/deployment/create', {
       method: 'POST',
       body: form,
-      // headers: {
-      //   'Access-Control-Allow-Origin': '*',
-      //   'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-      //   'Access-Control-Allow-Headers': 'Content-Type'
-      // }
     });
 
     if (response.ok) {
