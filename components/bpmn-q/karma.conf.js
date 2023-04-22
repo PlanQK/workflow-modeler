083514f1145ab117643b085c64ddec619ec87e44
Felix Belz <st155595@stud.uni-stuttgart.de>
2023-04-22 17:51:30 +0200

    Updated Github Action for running the tests

--- components/bpmn-q/karma.conf.js
+++ components/bpmn-q/karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://www.npmjs.com/search?q=keywords:karma-adapter
-    frameworks: ['mocha'],
+    frameworks: ['mocha', 'webpack'],
 
 
     // list of files / patterns to load in the browser
