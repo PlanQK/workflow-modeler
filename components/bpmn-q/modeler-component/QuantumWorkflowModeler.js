1cafe0599566865a500b69081662cca4caa8464c
Felix Belz <st155595@stud.uni-stuttgart.de>
2023-04-29 01:12:43 +0200

    Fixed vue bug

--- components/bpmn-q/modeler-component/QuantumWorkflowModeler.js
+++ components/bpmn-q/modeler-component/QuantumWorkflowModeler.js
@@ -1,16 +1,12 @@
-// import 'bpmn-js/dist/assets/diagram-js.css';
-// import 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
-// import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
-// import 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css';
-// import 'bpmn-js-properties-panel/dist/assets/element-templates.css';
-// import 'bpmn-js-properties-panel/dist/assets/properties-panel.css';
-// import './editor/resources/styling/editor-ui.css' ;
-// import './editor/resources/styling/modeler.css';
-// import './editor/ui/notifications/Notifications.css';
-// import './editor/ui/notifications/Notification';
-// import './common/camunda-components/styles/style.less';
-
-import bpmnFonts from './editor/resources/styling/bpmn-fonts.css';
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+import ButtonToolbar from "./editor/ui/ButtonToolbar";
+import {createNewDiagram, loadDiagram} from "./common/util/IoUtilities";
+import NotificationHandler from "./editor/ui/notifications/NotificationHandler";
+import {createModeler, getModeler} from "./editor/ModelerHandler";
+import {getPluginButtons, getStyles, getTransformationButtons} from "./editor/plugin/PluginHandler";
+import {setPluginConfig} from "./editor/plugin/PluginConfigHandler";
+import * as editorConfig from './editor/config/EditorConfigManager';
 
 import diagramJsStyle from 'bpmn-js/dist/assets/diagram-js.css';
 import bpmnEmbeddedStyle from 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
@@ -24,21 +20,9 @@ import notificationsStyle from './editor/ui/notifications/Notifications.css';
 import notificationStyle from './editor/ui/notifications/Notification.css';
 import configModal from './editor/config/config-modal.css';
 import lessStyle from './common/camunda-components/css-styles/style.css';
+
 import colors from './common/camunda-components/css-styles/_colors.css';
-
-// import allStyles from '../public/modeler-styles.css' ;
-
-import React from 'react';
-import {createRoot} from 'react-dom/client';
-import ButtonToolbar from "./editor/ui/ButtonToolbar";
-import {createNewDiagram, loadDiagram} from "./common/util/IoUtilities";
-import NotificationHandler from "./editor/ui/notifications/NotificationHandler";
-import {createModeler, getModeler} from "./editor/ModelerHandler";
-import {getPluginButtons, getStyles, getTransformationButtons} from "./editor/plugin/PluginHandler";
-import {setPluginConfig} from "./editor/plugin/PluginConfigHandler";
-import * as editorConfig from './editor/config/EditorConfigManager';
-
-export const notificationHandler = new NotificationHandler([]);
+import bpmnFonts from './editor/resources/styling/bpmn-fonts.css';
 
 class QuantumWorkflowModeler extends HTMLElement {
 
@@ -48,53 +32,19 @@ class QuantumWorkflowModeler extends HTMLElement {
     constructor() {
         super();
 
-        // import('./editor/resources/styling/bpmn-fonts.css')
-        // const fontEmbedded = document.createElement("link");
-        // fontEmbedded.setAttribute("rel", "stylesheet");
-        // fontEmbedded.setAttribute("href", './editor/resources/styling/bpmn-fonts.css');
-        // fontEmbedded.setAttribute("type", "text/html");
-
         // add css sheet which publishes the bpmn font to the head of the document
         let fontStyleTag = document.createElement('style');
         fontStyleTag.innerHTML = bpmnFonts;
         document.head.appendChild(fontStyleTag);
 
+        // add css color sheet to the head of the document
         let colorStyleTag = document.createElement('style');
         colorStyleTag.innerHTML = colors;
         document.head.appendChild(colorStyleTag);
-
-        // const fontEmbedded = document.createElement("link");
-        // fontEmbedded.setAttribute("rel", "stylesheet");
-        // fontEmbedded.setAttribute("href", 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css');
-        // fontEmbedded.setAttribute("type", "text/html");
-        //
-        // const fontCodes = document.createElement("link");
-        // fontCodes.setAttribute("rel", "stylesheet");
-        // fontCodes.setAttribute("href", 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css');
-        // fontCodes.setAttribute("type", "text/html");
-        //
-        //
-        // const fontBpmn = document.createElement("link");
-        // fontBpmn.setAttribute("rel", "stylesheet");
-        // fontBpmn.setAttribute("href", 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css');
-        // fontBpmn.setAttribute("type", "text/html");
-
-        // import bpmnEmbeddedStyle from 'bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css';
-        // import bpmnCodesStyle from 'bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css';
-        // import bpmnStyle from 'bpmn-js/dist/assets/bpmn-font/css/bpmn.css';
-        // document.head.appendChild(fontEmbedded);
-        // document.head.appendChild(fontCodes);
-        // document.head.appendChild(fontBpmn);
-
-        this.setShadowDOM();
     }
 
     connectedCallback() {
-        // this.setShadowDOM();
-
-        // const urlParams = new URLSearchParams(window.location.search);
-        // this.workflowModel = urlParams.get('workflow');
-        // this.startModeler();
+        this.setShadowDOM();
 
         const self = this;
         window.addEventListener("message", function (event) {
@@ -111,19 +61,14 @@ class QuantumWorkflowModeler extends HTMLElement {
             }
         });
 
-        // wait until shadow DOM is loaded
+        // wait until shadow dom is loaded
         requestAnimationFrame(() => {
-            // Get the container element from the shadow DOM
-
-            // Pass the container element to the BPMN JS modeler constructor
-            // const modeler = new BpmnJS({
-            //   container: bpmnContainer
-            // });
             this.startModeler();
         });
     }
 
     startModeler() {
+        console.log('Start Modeler');
 
         const bpmnContainer = this.shadowRoot.querySelector('#canvas');
         const propertiesPanelContainer = this.shadowRoot.querySelector('#properties');
@@ -132,6 +77,7 @@ class QuantumWorkflowModeler extends HTMLElement {
         propertiesPanelContainer.innerHTML = '';
 
         const modeler = createModeler(bpmnContainer, propertiesPanelContainer);
+        console.log('Created Modeler');
 
         const notificationsContainer = this.shadowRoot.querySelector('#qwm-notification-container');
 
@@ -140,6 +86,7 @@ class QuantumWorkflowModeler extends HTMLElement {
 
         const notificationRoot = createRoot(notificationsContainer);
         notificationRoot.render(<div>{notificationComponent}</div>);
+        console.log('Rendered Notifications React Component');
 
         // create a transformation button for each transformation method of a active plugin
         const transformationButtons = getTransformationButtons();
@@ -148,6 +95,7 @@ class QuantumWorkflowModeler extends HTMLElement {
         const root = createRoot(this.shadowRoot.querySelector('#button-container'));
         root.render(<ButtonToolbar modeler={modeler} pluginButtons={getPluginButtons()}
                                    transformButtons={transformationButtons}/>);
+        console.log('Rendered Toolbar React Component');
 
         if (this.workflowModel) {
             loadDiagram(this.workflowModel, getModeler());
@@ -168,17 +116,12 @@ class QuantumWorkflowModeler extends HTMLElement {
             this.shadowRoot = this.attachShadow({mode: "open"});
             editorConfig.setShadowRoot(this.shadowRoot);
         }
-        this.shadowRoot.innerHTML = `
-<!--              <style>-->
-<!--                @import url('./editor/resources/styling/modeler.css'); -->
-<!--              </style>-->
-`;
+        this.shadowRoot.innerHTML = ``;
 
         const div = document.createElement('div');
 
         // add styles as style tags to the shadow dom
-        // this.appendStyle(allStyles);
-        this.append(colors);
+        console.log('Start appending styles');
         this.appendStyle(lessStyle);
         this.appendStyle(bpmnEmbeddedStyle);
         this.appendStyle(bpmnCodesStyle);
@@ -197,51 +140,22 @@ class QuantumWorkflowModeler extends HTMLElement {
             this.appendStyle(styling);
         }
 
-        // styleTag = document.createElement('style');
-        // styleTag.innerHTML = editorStyle;
-        // this.shadowRoot.appendChild(styleTag);
-
+        console.log('Start div');
         div.setAttribute("style", "display: flex; flex-direction: column; height: 100%;");
         div.innerHTML = `
-<!--              <link rel="stylesheet" href="bpmn-js/dist/assets/diagram-js.css">-->
-<!--              <link rel="stylesheet" href="bpmn-js/dist/assets/bpmn-font/css/bpmn.css">-->
-<!--              <link rel="stylesheet" href="bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css">-->
-<!--              <link rel="stylesheet" href="bpmn-js/dist/assets/bpmn-font/css/bpmn-codes.css">-->
-<!--              <link rel="stylesheet" href="bpmn-js-properties-panel/dist/assets/element-templates.css">-->
-<!--              <link rel="stylesheet" href="bpmn-js-properties-panel/dist/assets/properties-panel.css">-->
-<!--              <link rel="stylesheet" href="./editor/resources/styling/modeler.css">-->
-<!--              <link rel="stylesheet" href="./editor/resources/styling/editor-ui.css">-->
-<!--              <link rel="stylesheet" href="./editor/ui/notifications/Notifications.css">-->
-<!--              <link rel="stylesheet" href="./editor/ui/notifications/Notifiation.css">-->
-<!--              <link rel="stylesheet" href="./common/camunda-components/styles/style.less">-->
-<!--              <link rel="stylesheet" href="./editor/resources/styling/modeler.css">-->
-<!--              <link rel="stylesheet" href="./editor/resources/styling/editor-ui.css">-->
-<!--              <style>-->
-<!--                @import url('./editor/resources/styling/modeler.css'); -->
-<!--              </style>-->
               <div id="button-container" style="flex-shrink: 0;"></div>
               <hr class="toolbar-splitter" />
               <div id="main-div" style="display: flex; flex: 1;">
                 <div id="canvas" style="width: 100%"></div>
                 <div id="properties" style="overflow: auto; max-height: 93.5vh; width: 25%; background: #f8f8f8;"></div>
               </div>
-              <div id="qwm-notification-container"></div>
-              
-`;
-        // var css = new CSSStyleSheet();
-        // // css.replaceSync( "@import url(./editor/resources/styling/modeler.css)" );
-        //
-        // css.replaceSync( editorStyle );
-        // this.shadowRoot.adoptedStyleSheets = [css];
-        //
-        //
-        // const linkElem = document.createElement("link");
-        // linkElem.setAttribute("rel", "stylesheet");
-        // linkElem.setAttribute("href", "./editor/resources/styling/modeler.css");
+              <div id="qwm-notification-container"></div>`;
 
-// Attach the created element to the shadow DOM
-//     this.shadowRoot.appendChild(linkElem);
+        console.log('Finished div');
+
+        // Attach the created element to the shadow DOM
         this.shadowRoot.appendChild(div);
+        console.log('append div');
     }
 
     appendStyle(style) {
@@ -264,15 +178,8 @@ class QuantumWorkflowModeler extends HTMLElement {
         // rerender shadow dom to add plugin elements
         this.setShadowDOM();
 
-        // restart modeler to apply plugin config
-        // this.startModeler();
+        // restart modeler to apply plugin config when shadow dom is rendered
         requestAnimationFrame(() => {
-            // Get the container element from the shadow DOM
-
-            // Pass the container element to the BPMN JS modeler constructor
-            // const modeler = new BpmnJS({
-            //   container: bpmnContainer
-            // });
             this.startModeler();
         });
     }
