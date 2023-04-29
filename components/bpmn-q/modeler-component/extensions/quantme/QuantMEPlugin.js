768918995d0eb95de5c35d13655b073d22d7675d
Felix Belz <st155595@stud.uni-stuttgart.de>
2023-04-29 11:48:53 +0200

    Update QuantMEPlugin.js
    Fixed merge bugs

--- components/bpmn-q/modeler-component/extensions/quantme/QuantMEPlugin.js
+++ components/bpmn-q/modeler-component/extensions/quantme/QuantMEPlugin.js
@@ -13,7 +13,7 @@ import HybridRuntimeTab from "./configTabs/HybridRuntimeTab";
 import NotificationHandler from "../../editor/ui/notifications/NotificationHandler";
 import {getQRMs} from "./qrm-manager";
 import {startQuantmeReplacementProcess} from "./replacement/QuantMETransformator";
-import {getXml, loadDiagram, saveXmlAsLocalFile} from "../../common/util/IoUtilities";
+import {getXml} from "../../common/util/IoUtilities";
 import {getModeler} from "../../editor/ModelerHandler";
 import * as camundaConfig from "../../editor/config/EditorConfigManager";
 import * as config from "./framework-config/config-manager";
@@ -23,74 +23,77 @@ import UpdateDataObjectConfigurationsButton from './configurations/UpdateDataObj
 
 import quantMEStyles from './styling/quantme.css';
 
-let quantMEModdleExtension = require('./resources/quantum4bpmn.json')
+let quantMEModdleExtension = require('./resources/quantum4bpmn.json');
 
 export default {
-  buttons: [<ExtensibleButton
-    subButtons={[<AdaptationPlugin/>, <QuantMEController/>, <UpdateQHAnaConfigurationsButton/>, <DeploymentPlugin/>]}
-    title="QuantME"
-    styleClass="quantme-logo"/>
-  ],
-  configTabs: [
-    {
-      tabId: 'DataConfigurationEndpointTab',
-      tabTitle: 'QuantME Data',
-      configTab: DataObjectConfigurationsTab,
-    },
-    {
-      tabId: 'OpenTOSCAEndpointTab',
-      tabTitle: 'OpenTOSCA',
-      configTab: OpenToscaTab,
-    },
-    {
-      tabId: 'BPMNTab',
-      tabTitle: 'Workflow',
-      configTab: BPMNConfigTab,
-    },
-    {
-      tabId: 'NISQAnalyzerEndpointTab',
-      tabTitle: 'NISQ Analyzer',
-      configTab: NisqAnalyzerTab,
-    },
-    {
-      tabId: 'QRMDataTab',
-      tabTitle: 'QRM Data',
-      configTab: QrmDataTab,
-    },
-    {
-      tabId: 'HybridRuntimesTab',
-      tabTitle: 'Hybrid Runtimes',
-      configTab: HybridRuntimeTab,
-    }],
-  name: 'quantme',
-  extensionModule: QuantMEExtensionModule,
-  moddleDescription: quantMEModdleExtension,
-  styling: [ quantMEStyles ],
-  transformExtensionButton: <TransformationButton name='QuantME Transformation' transformWorkflow={
-    async (xml) => {
-
-      // load current xml if not given as parameter
-      if (!xml) {
-        const modeler = getModeler();
-        xml = await getXml(modeler);
-      }
-
-      NotificationHandler.getInstance().displayNotification({
-        type: 'info',
-        title: 'Workflow Transformation Started!',
-        content: 'Successfully started transformation process for the current workflow!',
-        duration: 7000
-      });
-      // const modeler = getModeler();
-      //
-      // let xml = await modeler.get('bpmnjs').saveXML();
-      let currentQRMs = getQRMs();
-      return await startQuantmeReplacementProcess(xml, currentQRMs,
+    buttons: [<ExtensibleButton
+        subButtons={[<AdaptationPlugin/>, <QuantMEController/>, <UpdateDataObjectConfigurationsButton/>,
+            <DeploymentPlugin/>]}
+        title="QuantME"
+        styleClass="quantme-logo"/>
+    ],
+    configTabs: [
         {
-          nisqAnalyzerEndpoint: config.getNisqAnalyzerEndpoint(),
-          transformationFrameworkEndpoint: config.getTransformationFrameworkEndpoint(),
-          camundaEndpoint: camundaConfig.getCamundaEndpoint()
-        });
-    }
-  }/>,
+            tabId: 'DataConfigurationEndpointTab',
+            tabTitle: 'QuantME Data',
+            configTab: DataObjectConfigurationsTab,
+        },
+        {
+            tabId: 'OpenTOSCAEndpointTab',
+            tabTitle: 'OpenTOSCA',
+            configTab: OpenToscaTab,
+        },
+        {
+            tabId: 'BPMNTab',
+            tabTitle: 'Workflow',
+            configTab: BPMNConfigTab,
+        },
+        {
+            tabId: 'NISQAnalyzerEndpointTab',
+            tabTitle: 'NISQ Analyzer',
+            configTab: NisqAnalyzerTab,
+        },
+        {
+            tabId: 'QRMDataTab',
+            tabTitle: 'QRM Data',
+            configTab: QrmDataTab,
+        },
+        {
+            tabId: 'HybridRuntimesTab',
+            tabTitle: 'Hybrid Runtimes',
+            configTab: HybridRuntimeTab,
+        }
+    ],
+    name: 'quantme',
+    extensionModule: QuantMEExtensionModule,
+    moddleDescription: quantMEModdleExtension,
+    styling: [quantMEStyles],
+    transformExtensionButton: <TransformationButton name='QuantME Transformation' transformWorkflow={
+        async (xml) => {
+
+            // load current xml if not given as parameter
+            if (!xml) {
+                const modeler = getModeler();
+                xml = await getXml(modeler);
+            }
+
+            NotificationHandler.getInstance().displayNotification({
+                type: 'info',
+                title: 'Workflow Transformation Started!',
+                content: 'Successfully started transformation process for the current workflow!',
+                duration: 7000
+            });
+            // const modeler = getModeler();
+            //
+            // let xml = await modeler.get('bpmnjs').saveXML();
+            let currentQRMs = getQRMs();
+            return await startQuantmeReplacementProcess(xml, currentQRMs,
+                {
+                    nisqAnalyzerEndpoint: config.getNisqAnalyzerEndpoint(),
+                    transformationFrameworkEndpoint: config.getTransformationFrameworkEndpoint(),
+                    camundaEndpoint: camundaConfig.getCamundaEndpoint()
+                }
+            );
+        }
+    }/>,
 };
