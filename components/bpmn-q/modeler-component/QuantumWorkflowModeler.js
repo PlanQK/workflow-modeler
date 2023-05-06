2ca9c7044bfaa1293778cbc5605745611267e058
Felix Belz <st155595@stud.uni-stuttgart.de>
2023-05-06 14:45:28 +0200

    Added comments for the QuantumWorkflowModeler

--- components/bpmn-q/modeler-component/QuantumWorkflowModeler.js
+++ components/bpmn-q/modeler-component/QuantumWorkflowModeler.js
@@ -24,6 +24,11 @@ import lessStyle from './editor/resources/styling/camunda-styles/style.css';
 
 import bpmnFonts from './editor/resources/styling/bpmn-fonts.css';
 
+/**
+ * The Quantum Workflow modeler HTML web component which contains the bpmn-js modeler to model BPMN diagrams, an editor
+ * component for workflow editing functionality and plugins which add model extensions to the bpmn-js modeler and allow
+ * the modelling of quantum workflows.
+ */
 export class QuantumWorkflowModeler extends HTMLElement {
 
     workflowModel;
@@ -39,76 +44,42 @@ export class QuantumWorkflowModeler extends HTMLElement {
     }
 
     connectedCallback() {
+
+        // create the HTML structure of the component
         this.setShadowDOM();
 
+        // add listener for post messages containing a workflow to load into the modeler
         const self = this;
         window.addEventListener("message", function (event) {
 
+            // check if the message contains a correctly formated workflow
             if (event.origin === window.location.href.replace(/\/$/, '')
                 && event.data && event.data.workflow && typeof event.data.workflow === 'string' && event.data.workflow.startsWith('<?xml version="1.0" encoding="UTF-8"?>')) {
 
                 const xmlString = event.data.workflow;
                 self.workflowModel = xmlString;
-                editorConfig.setFileName(event.data.name);
 
+                // open sent workflow and save its file name
+                editorConfig.setFileName(event.data.name);
                 loadDiagram(xmlString, getModeler()).then();
             }
         });
 
         // wait until shadow dom is loaded
         requestAnimationFrame(() => {
+
+            // start the bpmn-js modeler and render the react components
             this.startModeler();
         });
     }
 
-    startModeler() {
-        console.log('Start Modeler');
 
-        initEditorEventHandler(this);
-
-        const bpmnContainer = this.shadowRoot.querySelector('#canvas');
-        const propertiesPanelContainer = this.shadowRoot.querySelector('#properties');
-
-        bpmnContainer.innerHTML = '';
-        propertiesPanelContainer.innerHTML = '';
-
-        const modeler = createModeler(bpmnContainer, propertiesPanelContainer);
-        console.log('Created Modeler');
-
-        const notificationsContainer = this.shadowRoot.querySelector('#qwm-notification-container');
-
-        const handler = NotificationHandler.getInstance();
-        const notificationComponent = handler.createNotificationsComponent([], notificationsContainer);
-
-        const notificationRoot = createRoot(notificationsContainer);
-        notificationRoot.render(<div>{notificationComponent}</div>);
-        console.log('Rendered Notifications React Component');
-
-        // create a transformation button for each transformation method of a active plugin
-        const transformationButtons = getTransformationButtons();
-
-        // integrate react components into the html component
-        const root = createRoot(this.shadowRoot.querySelector('#button-container'));
-        root.render(<ButtonToolbar modeler={modeler} pluginButtons={getPluginButtons()}
-                                   transformButtons={transformationButtons}/>);
-        console.log('Rendered Toolbar React Component');
-
-        this.workflowModel = this.workflowModel || getPluginConfig('editor').defaultWorkflow;
-        if (this.workflowModel) {
-            loadDiagram(this.workflowModel, getModeler()).then();
-        } else {
-            createNewDiagram(modeler);
-        }
-
-        const beforeUnloadListener = (event) => {
-            event.preventDefault();
-            return event.returnValue = '';
-        };
-        addEventListener("beforeunload", beforeUnloadListener, {capture: true});
-    }
-
+    /**
+     * Set up the inner structure of the component
+     */
     setShadowDOM() {
 
+        // create new shadow root if not already exists
         if (!this.shadowRoot) {
             this.shadowRoot = this.attachShadow({mode: "open"});
             editorConfig.setShadowRoot(this.shadowRoot);
@@ -138,6 +109,8 @@ export class QuantumWorkflowModeler extends HTMLElement {
         }
 
         console.log('Start div');
+
+        // define inner structure of the shadow DOM
         div.setAttribute("style", "display: flex; flex-direction: column; height: 100%;");
         div.innerHTML = `
               <div id="button-container" style="flex-shrink: 0;"></div>
@@ -154,12 +127,69 @@ export class QuantumWorkflowModeler extends HTMLElement {
         this.shadowRoot.appendChild(div);
     }
 
+    /**
+     * Initializes the modeler component by creating the bpmn-js modeler instance and rendering the React components of
+     * the editor into the DOM.
+     */
+    startModeler() {
+        console.log('Start Modeler');
+
+        // initialize event handler for workflow events with the instance of the component to dispatch the events correctly
+        initEditorEventHandler(this);
+
+        // get and reset the container in which the bpmn-js modeler and its properties panel should be rendered
+        const bpmnContainer = this.shadowRoot.querySelector('#canvas');
+        const propertiesPanelContainer = this.shadowRoot.querySelector('#properties');
+        bpmnContainer.innerHTML = '';
+        propertiesPanelContainer.innerHTML = '';
+
+        // create a new bpmn-js modeler instance with all additional modeules and extensions defined by the plugins
+        const modeler = createModeler(bpmnContainer, propertiesPanelContainer);
+        console.log('Created Modeler');
+
+        // setup the notification handler and render it into the DOM
+        const notificationsContainer = this.shadowRoot.querySelector('#qwm-notification-container');
+        const handler = NotificationHandler.getInstance();
+        const notificationComponent = handler.createNotificationsComponent([], notificationsContainer);
+
+        const notificationRoot = createRoot(notificationsContainer);
+        notificationRoot.render(<div>{notificationComponent}</div>);
+        console.log('Rendered Notifications React Component');
+
+        // create a transformation button for each transformation method of an active plugin
+        const transformationButtons = getTransformationButtons();
+
+        // integrate the React ButtonToolbar into its DOM container
+        const root = createRoot(this.shadowRoot.querySelector('#button-container'));
+        root.render(<ButtonToolbar modeler={modeler} pluginButtons={getPluginButtons()}
+                                   transformButtons={transformationButtons}/>);
+
+        // load initial workflow
+        this.workflowModel = this.workflowModel || getPluginConfig('editor').defaultWorkflow;
+        if (this.workflowModel) {
+            loadDiagram(this.workflowModel, getModeler()).then();
+        } else {
+            createNewDiagram(modeler);
+        }
+    }
+
+    /**
+     * Create a style tag containing the styling of the given style and add the style tag to the shadow DOM
+     *
+     * @param style The given style
+     */
     appendStyle(style) {
         let styleTag = document.createElement('style');
         styleTag.innerHTML = style;
         this.shadowRoot.appendChild(styleTag);
     }
 
+    /**
+     * Load the given xml string as a workflow into the modeler.
+     *
+     * @param xmlDiagram The workflow to load as xml string
+     * @return {Promise<*|undefined>}
+     */
     async loadWorkflowDiagram(xmlDiagram) {
         const modeler = getModeler();
 
@@ -171,15 +201,27 @@ export class QuantumWorkflowModeler extends HTMLElement {
 
     }
 
+    /**
+     * Getter for the plugin config of the Quantum Workflow Modeler
+     *
+     * @return {*[]} The plugin config as an array of {name: string, (optional) config: {}}
+     */
     get pluginConfigs() {
         return this.pluginConfigsList || [];
     }
 
+    /**
+     * Setter for the plugin config of the Quantum Workflow Modeler
+     *
+     * @param pluginConfigs The plugin config as an array of {name: string, (optional) config: {}}
+     */
     set pluginConfigs(pluginConfigs) {
         console.log(pluginConfigs);
         this.pluginConfigsList = pluginConfigs;
         const configs = this.pluginConfigsList;
         console.log(configs);
+
+        // add plugin config to the PluginConfigHandler
         setPluginConfig(configs);
 
         // rerender shadow dom to add plugin elements
