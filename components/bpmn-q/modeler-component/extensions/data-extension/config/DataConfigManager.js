c63d769de53596d13b772b6a793646a488eb67e3
LaviniaStiliadou <livia_16@live.de>
2023-05-16 17:44:14 +0200

    Feature/environment variables (#41)
    * add configuration with environment variables
    
    * enable configuration also for qhana, editor and data
    
    * fix error in transformation test
    
    * enable to activate/ deactivate plugins per variable
    
    ---------
    
    Co-authored-by: SharonNaemi <naemi_17@live.de>

--- components/bpmn-q/modeler-component/extensions/data-extension/config/DataConfigManager.js
+++ components/bpmn-q/modeler-component/extensions/data-extension/config/DataConfigManager.js
@@ -2,7 +2,7 @@ import {getPluginConfig} from "../../../editor/plugin/PluginConfigHandler";
 
 // default config entries used if no value is specified in the initial plugin config
 const defaultConfig = {
-    configurationsEndpoint: 'http://localhost:8000/service-task',
+    configurationsEndpoint: process.env.SERVICE_DATA_CONFIG
 };
 
 // current config
@@ -14,7 +14,7 @@ let config = {};
  * @return {string} the currently specified endpoint url of the Configurations endpoint
  */
 export function getConfigurationsEndpoint() {
-    if (!config.configurationsEndpoint) {
+    if (config.configurationsEndpoint === undefined) {
         setConfigurationsEndpoint(getPluginConfig('dataflow').configurationsEndpoint || defaultConfig.configurationsEndpoint);
     }
     return config.configurationsEndpoint;
