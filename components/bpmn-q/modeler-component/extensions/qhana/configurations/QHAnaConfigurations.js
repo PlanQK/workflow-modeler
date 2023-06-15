c0f469652f50af54102b5f96dcfd3c3115531881
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-06-15 16:07:27 +0200

    Enable unnecessary espace rule

--- components/bpmn-q/modeler-component/extensions/qhana/configurations/QHAnaConfigurations.js
+++ components/bpmn-q/modeler-component/extensions/qhana/configurations/QHAnaConfigurations.js
@@ -199,7 +199,7 @@ export function createConfigurationForServiceData(serviceData) {
       name: "qoutput.output_" + index,
       label: "qoutput.output_" + index,
       type: "String",
-      value: `\$\{output\}`, //JSON.stringify(value),
+      value: `$\{output}`, //JSON.stringify(value),
       hide: true,
       bindTo: {
         name: "outputParameters",
