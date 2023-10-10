b2a390581edb33c8fec37eb0767bfbf9ca910089
LaviniaStiliadou <livia_16@live.de>
2023-10-10 16:39:08 +0200

    align buttons horizontally

--- components/bpmn-q/modeler-component/extensions/pattern/ui/adaptation/AdaptationModal.js
+++ components/bpmn-q/modeler-component/extensions/pattern/ui/adaptation/AdaptationModal.js
@@ -10,13 +10,13 @@ export default function AdaptationModal({ onClose, responseData }) {
 
   const [currentView, setCurrentView] = useState("algorithmic");
   const [dynamicButtons, setDynamicButtons] = useState([{ label: "Default Button", viewType: "default" }, { label: "Algorithmic Patterns", viewType: "algorithmic" }]);
-  const [buttonSelectedPatterns, setButtonSelectedPatterns] = useState({}); // State to store selected patterns for each button
-  const [patternsToDisplay, setPatternsToDisplay] = useState([]); // State to store the patterns to display for all buttons
+  const [buttonSelectedPatterns, setButtonSelectedPatterns] = useState({});
+  const [patternsToDisplay, setPatternsToDisplay] = useState([]);
 
   const addDynamicButton = () => {
     const newButtonLabel = `Button ${dynamicButtons.length + 1}`;
     setDynamicButtons([...dynamicButtons, { label: newButtonLabel, viewType: "dynamic" }]);
-    setButtonSelectedPatterns({ ...buttonSelectedPatterns, [newButtonLabel]: [] }); // Initialize selected patterns for the new button
+    setButtonSelectedPatterns({ ...buttonSelectedPatterns, [newButtonLabel]: [] });
   };
 
   const switchView = (viewType) => {
@@ -41,13 +41,12 @@ export default function AdaptationModal({ onClose, responseData }) {
   const handleButtonDrag = (dragIndex, hoverIndex) => {
     const draggedButton = dynamicButtons[dragIndex];
     const updatedButtons = [...dynamicButtons];
-    updatedButtons.splice(dragIndex, 1); // Remove the dragged button
-    updatedButtons.splice(hoverIndex, 0, draggedButton); // Insert the dragged button at the new position
+    updatedButtons.splice(dragIndex, 1);
+    updatedButtons.splice(hoverIndex, 0, draggedButton);
     setDynamicButtons(updatedButtons);
   };
 
   useEffect(() => {
-    // Determine which patterns to display based on the current view
     if (currentView === "algorithmic") {
       setPatternsToDisplay(responseData);
     } else if (currentView === "behavior") {
@@ -63,29 +62,31 @@ export default function AdaptationModal({ onClose, responseData }) {
 
       <Body>
         <div className="pattern-type-buttons">
-          {dynamicButtons.map((button, index) => (
-            <div
-              key={index}
-              draggable={true}
-              onDragStart={(e) => {
-                e.dataTransfer.setData("index", index);
-              }}
-              onDragOver={(e) => e.preventDefault()}
-              onDrop={(e) => {
-                e.preventDefault();
-                const dragIndex = e.dataTransfer.getData("index");
-                const hoverIndex = index;
-                handleButtonDrag(dragIndex, hoverIndex);
-              }}
-            >
-              <button onClick={() => switchView(button.viewType)}>{button.label}</button>
-            </div>
-          ))}
+          <div className="dynamic-buttons-container">
+            {dynamicButtons.map((button, index) => (
+              <div
+                key={index}
+                draggable={true}
+                onDragStart={(e) => {
+                  e.dataTransfer.setData("index", index);
+                }}
+                onDragOver={(e) => e.preventDefault()}
+                onDrop={(e) => {
+                  e.preventDefault();
+                  const dragIndex = e.dataTransfer.getData("index");
+                  const hoverIndex = index;
+                  handleButtonDrag(dragIndex, hoverIndex);
+                }}
+              >
+                <button onClick={() => switchView(button.viewType)}>{button.label}</button>
+              </div>
+            ))}
+          </div>
           <button onClick={addDynamicButton}>+</button>
         </div>
         <div className="image-container" style={{ display: "flex", flexWrap: "wrap", justifyContent: "flex-start" }}>
           {patternsToDisplay.map((pattern, index) => {
-            const buttonLabel = dynamicButtons[index]?.label || ''; // Retrieve button label or use an empty string as a default
+            const buttonLabel = dynamicButtons[index]?.label || "";
             return (
               <div
                 key={index}
