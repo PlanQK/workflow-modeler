1b8dc1ab933fc3a73db08bb7249bef6ba9fbf600
LaviniaStiliadou <livia_16@live.de>
2023-10-10 14:19:01 +0200

    scale image

--- components/bpmn-q/modeler-component/extensions/pattern/ui/adaptation/AdaptationModal.js
+++ components/bpmn-q/modeler-component/extensions/pattern/ui/adaptation/AdaptationModal.js
@@ -1,16 +1,4 @@
-/**
- * Copyright (c) 2023 Institute of Architecture of Application Systems -
- * University of Stuttgart
- *
- * This program and the accompanying materials are made available under the
- * terms the Apache Software License 2.0
- * which is available at https://www.apache.org/licenses/LICENSE-2.0.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-/* eslint-disable no-unused-vars */
-import React, {useState, useEffect} from "react";
+import React, { useState } from "react";
 import Modal from "../../../../editor/ui/modal/Modal";
 
 // polyfill upcoming structural components
@@ -23,33 +11,80 @@ const Footer = Modal.Footer || (({ children }) => <div>{children}</div>);
  * and improve it if necessary.
  *
  * @param onClose Callback called when the modal is closed
+ * @param responseData Data received from the API
  * @return {JSX.Element}
  * @constructor
  */
 export default function AdaptationModal({ onClose, responseData }) {
-  console.log(responseData)
-  return (<Modal onClose={onClose}>
-    <Title>
-    Adaptation Modal
-    </Title>
+  console.log(responseData);
 
-    <Body>
-    <Body>
-  {responseData && responseData.map((pattern, index) => (
-    <div key={index}>
-      <h2>{pattern.name}</h2>
-      <img src={pattern.iconUrl} alt={pattern.name} />
-      {/* Add more content here */}
-    </div>
-  ))}
-</Body>
+  const [currentView, setCurrentView] = useState("algorithmic"); // Initially, display Algorithmic Patterns
 
-    </Body>
+  // Separate patterns based on their types
+  const algorithmicPatterns = responseData;
+  const behaviorPatterns = responseData.filter((pattern) => pattern.type === "behavior");
+  const morePatterns = responseData.filter((pattern) => pattern.type === "more");
 
-    <Footer>
-        <div id="hybridLoopAdaptationFormButtons">
-            <button type="button" className="qwm-btn qwm-btn-secondary" onClick={() => onClose()}>Cancel</button>
+  return (
+    <Modal onClose={onClose}>
+      <Title>Pattern Selection</Title>
+
+      <Body>
+        {/* Buttons/Tabs to switch between views */}
+        <div className="pattern-type-buttons">
+          <button onClick={() => setCurrentView("algorithmic")}>Algorithmic Patterns</button>
+          <button onClick={() => setCurrentView("behavior")}>Behavior Patterns</button>
+          <button onClick={() => setCurrentView("more")}>More Patterns</button>
         </div>
-    </Footer>
-</Modal>)
+
+        {/* Render the selected view */}
+        {currentView === "algorithmic" && algorithmicPatterns.length > 0 && (
+          <div>
+            <h3>Algorithmic Patterns</h3>
+            {algorithmicPatterns.map((pattern, index) => (
+              <div key={index}>
+                <h4>{pattern.name}</h4>
+                <img src={pattern.iconUrl} alt={pattern.name} style={{ width: "25%", height: "auto" }} />
+                {/* Add more content specific to algorithmic patterns */}
+              </div>
+            ))}
+          </div>
+        )}
+
+        {currentView === "behavior" && behaviorPatterns.length > 0 && (
+          <div>
+            <h3>Behavior Patterns</h3>
+            {behaviorPatterns.map((pattern, index) => (
+              <div key={index}>
+                <h2>{pattern.name}</h2>
+                <img src={pattern.iconUrl} alt={pattern.name} />
+                {/* Add more content specific to behavior patterns */}
+              </div>
+            ))}
+          </div>
+        )}
+
+        {currentView === "more" && morePatterns.length > 0 && (
+          <div>
+            <h3>More Patterns</h3>
+            {morePatterns.map((pattern, index) => (
+              <div key={index}>
+                <h2>{pattern.name}</h2>
+                <img src={pattern.iconUrl} alt={pattern.name} />
+                {/* Add more content specific to more patterns */}
+              </div>
+            ))}
+          </div>
+        )}
+      </Body>
+
+      <Footer>
+        <div id="hybridLoopAdaptationFormButtons">
+          <button type="button" className="qwm-btn qwm-btn-secondary" onClick={() => onClose()}>
+            Cancel
+          </button>
+        </div>
+      </Footer>
+    </Modal>
+  );
 }
