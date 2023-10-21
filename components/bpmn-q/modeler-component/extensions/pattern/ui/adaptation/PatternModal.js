3beaef80bc29cd04a34dce7edf43067880b891f8
LaviniaStiliadou <livia_16@live.de>
2023-10-21 19:19:30 +0200

    update text

--- components/bpmn-q/modeler-component/extensions/pattern/ui/adaptation/PatternModal.js
+++ components/bpmn-q/modeler-component/extensions/pattern/ui/adaptation/PatternModal.js
@@ -11,10 +11,22 @@ export default function PatternModal({ onClose}) {
       <Title>Pattern Selection</Title>
 
       <Body>
-        This wizard guides you through the selection of patterns to automatically generate quantum workflows.
-        Algorithmic: Select an algorithm for which a quantum workflow should be generated/loaded/adapted
-        Behavioral: Select how the algorithm should be executed.
-        Augmentation: Select which further features should be added to improve the execution.
+      <h3 className="spaceUnder">
+      This wizard guides you through the selection of patterns to automatically generate quantum workflows.
+        </h3>
+        <div className="spaceUnder">
+        The pattern selection enable to automatically generate quantum workflows.Thereby, three different types of pattern exists:
+        The algorithmic pattern specifies which algorithm should be used, loaded, or adapted within the quantum workflow.
+        It involves selecting the algorithm that is best suited to solving the given problem.
+        The behavioral pattern determines the execution strategy for the chosen algorithm.
+        It dictates how the algorithm should be run to optimize the utilization of quantum resources and enhance performance.
+        The augmentation pattern allows for the enhancement of the quantum workflow by incorporating additional features or elements. These features are selected to improve the execution of the algorithm, further optimizing its performance and utility in solving the problem.
+        Further information,
+          as well as current restrictions, can be found in the{" "}
+          <a href="https://www.quantumcomputingpatterns.org">
+            documentation
+          </a>
+         </div>
       </Body>
 
       <Footer>
