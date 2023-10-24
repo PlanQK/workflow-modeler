7240c7046f4a7dd932f0a20439048514a1e5d786
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-10-24 12:53:16 +0200

    Move deployment models to separate group to enable combining them with connectors

---
+++ components/bpmn-q/modeler-component/extensions/opentosca/modeling/properties-provider/DeploymentModelProps.js
@@ -0,0 +1,94 @@
+/**
+ * Copyright (c) 2023 Institute of Architecture of Application Systems -
+ * University of Stuttgart
+ *
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { getServiceTaskLikeBusinessObject } from "../../../../editor/util/camunda-utils/ImplementationTypeUtils";
+import { ArtifactUpload } from "./ArtifactUpload";
+import { isTextFieldEntryEdited } from "@bpmn-io/properties-panel";
+import { Deployment } from "./Deployment";
+import { YamlUpload } from "./YamlUpload";
+import { Connector } from "./Connector";
+import yaml from "js-yaml";
+
+const QUANTME_NAMESPACE_PULL = "http://quantil.org/quantme/pull";
+
+/**
+ * Properties group for service tasks. Extends the original implementation by adding a new selection option to the
+ * implementation entry: deployment.
+ *
+ * @param props
+ * @return {{component: function(*): preact.VNode<any>, isEdited: function(*): *, id: string}[]|*[]}
+ * @constructor
+ */
+export function DeploymentModelProps(props) {
+  const { element, wineryEndpoint, translate } = props;
+
+  // deployment models can only be defined for ServiceTasks
+  if (!getServiceTaskLikeBusinessObject(element)) {
+    return [];
+  }
+
+  // list of configuration options
+  const entries = [];
+
+  // field to define deployment models
+  entries.push({
+    id: "deployment",
+    element,
+    translate,
+    wineryEndpoint,
+    component: Deployment,
+    isEdited: isTextFieldEntryEdited,
+  });
+
+  // field to upload an OpenAPI spec for automated connector generation
+  entries.push({
+    id: "yamlUpload",
+    component: YamlUpload,
+    isEdited: isTextFieldEntryEdited,
+  });
+  if (
+    element.businessObject.deploymentModelUrl &&
+    !element.businessObject.deploymentModelUrl.includes(
+      encodeURIComponent(encodeURIComponent(QUANTME_NAMESPACE_PULL))
+    ) &&
+    element.businessObject.yaml !== undefined
+  ) {
+    const urls = extractUrlsFromYaml(element.businessObject.yaml);
+    entries.push({
+      id: "connector",
+      element,
+      translate,
+      urls,
+      component: Connector,
+      isEdited: isTextFieldEntryEdited,
+    });
+  }
+
+  entries.push({
+    id: "artifactUpload",
+    element,
+    translate,
+    component: ArtifactUpload,
+    isEdited: isTextFieldEntryEdited,
+  });
+
+  return entries;
+}
+
+function extractUrlsFromYaml(content) {
+  const doc = yaml.load(content);
+
+  // Extract URLs from paths
+  const paths = Object.keys(doc.paths);
+  return paths.map((path) => {
+    return `${path}`;
+  });
+}
--- components/bpmn-q/modeler-component/extensions/opentosca/modeling/properties-provider/ImplementationProps.js
+++ components/bpmn-q/modeler-component/extensions/opentosca/modeling/properties-provider/ImplementationProps.js
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2023 Institute of Architecture of Application Systems -
+ * University of Stuttgart
+ *
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
 import {
   isTextFieldEntryEdited,
   TextFieldEntry,
@@ -8,24 +19,16 @@ import { useService } from "bpmn-js-properties-panel";
 import { getImplementationType } from "../../../quantme/utilities/ImplementationTypeHelperExtension";
 import { getServiceTaskLikeBusinessObject } from "../../../../editor/util/camunda-utils/ImplementationTypeUtils";
 import { getExtensionElementsList } from "../../../../editor/util/camunda-utils/ExtensionElementsUtil";
-import { Deployment } from "./Deployment";
-import { Connector } from "./Connector";
-import { YamlUpload } from "./YamlUpload";
-import { ArtifactUpload } from "./ArtifactUpload";
-
-const yaml = require("js-yaml");
-const QUANTME_NAMESPACE_PULL = "http://quantil.org/quantme/pull";
 
 /**
- * Properties group for service tasks. Extends the original implementation by adding a new selection option to the
- * implementation entry: deployment.
+ * Properties group for implementations of service tasks.
  *
- * @param props
+ * @param element
  * @return {{component: function(*): preact.VNode<any>, isEdited: function(*): *, id: string}[]|*[]}
  * @constructor
  */
-export function ImplementationProps(props) {
-  const { element, wineryEndpoint, translate } = props;
+export function ImplementationProps(element) {
+  console.log("Rendering implementation properties for ServiceTask!");
 
   if (!getServiceTaskLikeBusinessObject(element)) {
     return [];
@@ -76,58 +79,9 @@ export function ImplementationProps(props) {
       component: ConnectorId,
       isEdited: isTextFieldEntryEdited,
     });
-
-    // custom extension
-  } else if (implementationType === "deploymentModel") {
-    entries.push({
-      id: "deployment",
-      element,
-      translate,
-      wineryEndpoint,
-      component: Deployment,
-      isEdited: isTextFieldEntryEdited,
-    });
-    entries.push({
-      id: "yamlUpload",
-      component: YamlUpload,
-      isEdited: isTextFieldEntryEdited,
-    });
-    if (
-      !element.businessObject.deploymentModelUrl.includes(
-        encodeURIComponent(encodeURIComponent(QUANTME_NAMESPACE_PULL))
-      ) &&
-      element.businessObject.yaml !== undefined
-    ) {
-      const urls = extractUrlsFromYaml(element.businessObject.yaml);
-      entries.push({
-        id: "connector",
-        element,
-        translate,
-        urls,
-        component: Connector,
-        isEdited: isTextFieldEntryEdited,
-      });
-    }
-    entries.push({
-      id: "artifactUpload",
-      element,
-      translate,
-      component: ArtifactUpload,
-      isEdited: isTextFieldEntryEdited,
-    });
   }
 
   return entries;
-}
-
-function extractUrlsFromYaml(content) {
-  const doc = yaml.load(content);
-
-  // Extract URLs from paths
-  const paths = Object.keys(doc.paths);
-  return paths.map((path) => {
-    return `${path}`;
-  });
 }
 
 export function JavaClass(props) {
