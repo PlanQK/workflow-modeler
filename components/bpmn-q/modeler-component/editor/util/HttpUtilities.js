b8980d75a375bea101d43eb354c693f1b8f3502e
LaviniaStiliadou <livia_16@live.de>
2023-10-27 15:16:14 +0200

    Enable pattern selection (#108)
    * Add linter script and fix errors
    
    * Apply prettier
    
    * Add workflow executing linters
    
    * Add working directory for npm ci
    
    * Remove unknown working-directory from linting action
    
    * Update linting workflow
    
    * Fix linting command
    
    * Fix NOTICE file
    
    * Fix licence years
    
    * Fix name in Github workflow
    
    * Enable unused variable linting rule
    
    * Run prettier
    
    * Enable unnecessary espace rule
    
    * Add .eslintignore
    
    * Adapt path to public folder
    
    * Update ignore rules for linters
    
    * Lint changes after merging master
    
    * Start fixing Readme
    
    * Lint changes from merge
    
    * Lint master merge
    
    * Lint changes after merge with origin/master
    
    * Replace renderer with correct file
    
    * Lint changes
    
    * Lint changes
    
    * Run linter
    
    * Fix lint error
    
    * Add badges to readme
    
    * New diagram dialog (#89)
    
    * dialog for new diagram, isExecutable to true
    
    Co-Authored-By: LaviniaStiliadou <livia_16@live.de>
    
    * fix lint
    
    * adjust label for canceling dialog
    
    ---------
    
    Co-authored-by: SharonNaemi <naemi_17@live.de>
    Co-authored-by: mbeisel <beiselmn@gmail.com>
    
    * Fix plugin handling (#87)
    
    * fix plugin handling
    
    * fix lint
    
    * add plugin pattern
    
    * scale image
    
    * allow to select patterns
    
    * store patterns correctly with each button
    
    * allow drag & drop
    
    * align buttons horizontally
    
    * fix layout of button, add endpointurl
    
    * open modal to select algorithmic patterns
    
    * fix selecting patterns to algorithmic patterns
    
    * fix warning when setting endpoint
    
    * add error message when endpoint is incorrect
    
    * update text
    
    * remove unused classes, change icon
    
    * fix selected patterns for modal
    
    * modify selection modal
    
    * change position
    
    * add delete
    
    * add message to select algorithm pattern
    
    * fix edit
    
    * fix selected content
    
    * remove quantme from pattern plugin
    
    * change adaptation to pattern selection
    
    * remove files
    
    * fix layout
    
    * fix lint
    
    * return result
    
    * fix tests
    
    * adjust + button position
    
    * disable button if response is invalid
    
    * Fix comment for PatternConfigTab
    
    * Fix comments for config-manager.js
    
    * Change button style
    
    * Remove comment
    
    * Remove unused methods
    
    * Refactor http fetch function to general utility class
    
    * remove comments
    
    * Remove unused methods
    
    * Add copyright statements
    
    * Remove wrong comment
    
    * change icon
    
    * fix lint
    
    * Small changes for pattern intro text
    
    * Remove comma
    
    * Inline variables
    
    * Fix button naming
    
    * Move pattern categories to constants
    
    * Remove pattern.json
    
    * Update default config
    
    * Activate plugins for tests
    
    ---------
    
    Co-authored-by: Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
    Co-authored-by: SharonNaemi <naemi_17@live.de>
    Co-authored-by: mbeisel <beiselmn@gmail.com>

---
+++ components/bpmn-q/modeler-component/editor/util/HttpUtilities.js
@@ -0,0 +1,30 @@
+/**
+ * Copyright (c) 2023 Institute of Architecture of Application Systems -
+ * University of Stuttgart
+ *
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+/**
+ * Retrieves the Json data from the given endpoint.
+ *
+ * @param endpoint the endpoint to retrieve the data form
+ * @returns
+ */
+export async function fetchDataFromEndpoint(endpoint) {
+  try {
+    const response = await fetch(endpoint);
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {};
+  }
+}
---
+++ components/bpmn-q/modeler-component/extensions/pattern/ui/pattern-selection/PatternSelectionModal.js
@@ -0,0 +1,248 @@
+/**
+ * Copyright (c) 2023 Institute of Architecture of Application Systems -
+ * University of Stuttgart
+ *
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import React, { useState, useEffect } from "react";
+import Modal from "../../../../editor/ui/modal/Modal";
+import {
+  PATTERN_BEHAVIORAL,
+  PATTERN_ALGORITHM,
+  PATTERN_AUGMENTATION,
+} from "../../Constants";
+
+const Title = Modal.Title || (({ children }) => <h4>{children}</h4>);
+const Body = Modal.Body || (({ children }) => <div>{children}</div>);
+const Footer = Modal.Footer || (({ children }) => <div>{children}</div>);
+
+export default function PatternSelectionModal({
+  patterns,
+  onSelectPattern,
+  onClose,
+  initialSelectedPattern,
+}) {
+  const [selectedAlgorithmPattern, setSelectedAlgorithmPattern] =
+    useState(null);
+  const [selectedBehavioralPatterns, setSelectedBehavioralPatterns] = useState(
+    []
+  );
+  const [selectedAugmentationPatterns, setSelectedAugmentationPatterns] =
+    useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  useEffect(() => {
+    if (initialSelectedPattern) {
+      setSelectedAlgorithmPattern(initialSelectedPattern.algorithmPattern);
+      setSelectedBehavioralPatterns(initialSelectedPattern.behavioralPattern);
+      setSelectedAugmentationPatterns(
+        initialSelectedPattern.augmentationPattern
+      );
+    }
+  }, [initialSelectedPattern]);
+
+  const handlePatternSelection = (pattern, category) => {
+    if (category === PATTERN_ALGORITHM) {
+      setSelectedAlgorithmPattern(pattern);
+    } else if (category === PATTERN_BEHAVIORAL) {
+      setSelectedBehavioralPatterns((prevSelected) => {
+        if (prevSelected.includes(pattern)) {
+          return prevSelected.filter((selected) => selected !== pattern);
+        } else {
+          return [...prevSelected, pattern];
+        }
+      });
+    } else if (category === PATTERN_AUGMENTATION) {
+      setSelectedAugmentationPatterns((prevSelected) => {
+        if (prevSelected.includes(pattern)) {
+          return prevSelected.filter((selected) => selected !== pattern);
+        } else {
+          return [...prevSelected, pattern];
+        }
+      });
+    }
+  };
+
+  const handleConfirmSelection = () => {
+    if (selectedAlgorithmPattern) {
+      const selectedPatterns = {
+        algorithm: selectedAlgorithmPattern,
+        behavioral: selectedBehavioralPatterns,
+        augmentation: selectedAugmentationPatterns,
+      };
+      onSelectPattern(selectedPatterns);
+      onClose();
+      clearErrorMessage();
+    } else {
+      setErrorMessage("Please select an algorithm pattern before confirming.");
+    }
+  };
+
+  const clearErrorMessage = () => {
+    setErrorMessage("");
+  };
+
+  const algorithmPatterns = patterns.filter((pattern) =>
+    pattern.tags.includes(PATTERN_ALGORITHM)
+  );
+  const behavioralPatterns = patterns.filter((pattern) =>
+    pattern.tags.includes(PATTERN_BEHAVIORAL)
+  );
+  const augmentationPatterns = patterns.filter((pattern) =>
+    pattern.tags.includes(PATTERN_AUGMENTATION)
+  );
+
+  return (
+    <Modal onClose={onClose}>
+      <Title>Patterns</Title>
+      <Body>
+        {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+        <div>
+          <h3>Algorithm Patterns</h3>
+          <table>
+            <colgroup>
+              <col span="1" style={{ width: "30%" }} />
+              <col span="1" style={{ width: "15%" }} />
+              <col span="1" style={{ width: "7%" }} />
+            </colgroup>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Icon</th>
+                <th>Select</th>
+              </tr>
+            </thead>
+            <tbody>
+              {algorithmPatterns.map((pattern) => (
+                <tr key={pattern.id}>
+                  <td>{pattern.name}</td>
+                  <td>
+                    <img
+                      src={pattern.iconUrl}
+                      alt={pattern.name}
+                      style={{ width: "30%", height: "auto" }}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="radio"
+                      name="algorithm-pattern"
+                      checked={selectedAlgorithmPattern === pattern}
+                      onChange={() =>
+                        handlePatternSelection(pattern, PATTERN_ALGORITHM)
+                      }
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <h3>Behavioral Patterns</h3>
+          <table>
+            <colgroup>
+              <col span="1" style={{ width: "30%" }} />
+              <col span="1" style={{ width: "15%" }} />
+              <col span="1" style={{ width: "7%" }} />
+            </colgroup>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Icon</th>
+                <th>Select</th>
+              </tr>
+            </thead>
+            <tbody>
+              {behavioralPatterns.map((pattern) => (
+                <tr key={pattern.id}>
+                  <td>{pattern.name}</td>
+                  <td>
+                    <img
+                      src={pattern.iconUrl}
+                      alt={pattern.name}
+                      style={{ width: "30%", height: "auto" }}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={selectedBehavioralPatterns.includes(pattern)}
+                      onChange={() =>
+                        handlePatternSelection(pattern, PATTERN_BEHAVIORAL)
+                      }
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <h3>Augmentation Patterns</h3>
+          <table>
+            <colgroup>
+              <col span="1" style={{ width: "30%" }} />
+              <col span="1" style={{ width: "15%" }} />
+              <col span="1" style={{ width: "7%" }} />
+            </colgroup>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Icon</th>
+                <th>Select</th>
+              </tr>
+            </thead>
+            <tbody>
+              {augmentationPatterns.map((pattern) => (
+                <tr key={pattern.id}>
+                  <td>{pattern.name}</td>
+                  <td>
+                    <img
+                      src={pattern.iconUrl}
+                      alt={pattern.name}
+                      style={{ width: "30%", height: "auto" }}
+                    />
+                  </td>
+                  <td>
+                    <input
+                      type="checkbox"
+                      checked={selectedAugmentationPatterns.includes(pattern)}
+                      onChange={() =>
+                        handlePatternSelection(pattern, PATTERN_AUGMENTATION)
+                      }
+                    />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+        <Footer>
+          <div id="configFormButtons">
+            <button
+              className="qwm-btn qwm-btn-primary"
+              onClick={handleConfirmSelection}
+            >
+              Confirm Selection
+            </button>
+
+            <button
+              type="button"
+              className="qwm-btn qwm-btn-secondary"
+              onClick={() => onClose()}
+            >
+              Cancel
+            </button>
+          </div>
+        </Footer>
+      </Body>
+    </Modal>
+  );
+}
