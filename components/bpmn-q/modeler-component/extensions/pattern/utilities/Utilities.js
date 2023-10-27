b88af96ef12ce94bcb1f55de0997ec4508cdbaba
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-10-27 12:41:55 +0200

    Remove unused methods

--- components/bpmn-q/modeler-component/extensions/pattern/utilities/Utilities.js
+++ components/bpmn-q/modeler-component/extensions/pattern/utilities/Utilities.js
@@ -9,49 +9,6 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import $ from "jquery";
-
-/**
- * Check if the given task is a QuantME task
- *
- * @param task the task to check
- * @returns true if the passed task is a QuantME task, false otherwise
- */
-export function isQuantMETask(task) {
-  return task.$type.startsWith("quantme:");
-}
-
-/**
- * Return all QuantumCircuitExecutionTasks from the given list of modeling elements
- *
- * @param modelingElements the list of modeling elements
- * @return the list of contained QuantumCircuitExecutionTasks
- */
-export function getQuantumCircuitExecutionTasks(modelingElements) {
-  return modelingElements.filter(
-    (element) => element.$type === "quantme:QuantumCircuitExecutionTask"
-  );
-}
-
-export function performAjax(targetUrl, dataToSend) {
-  return new Promise(function (resolve, reject) {
-    $.ajax({
-      type: "POST",
-      url: targetUrl,
-      data: dataToSend,
-      processData: false,
-      contentType: false,
-      beforeSend: function () {},
-      success: function (data) {
-        resolve(data);
-      },
-      error: function (err) {
-        reject(err);
-      },
-    });
-  });
-}
-
 /**
  * Retrieves the pattern from the pattern atlas endpoint.
  * @param patternEndpoint
