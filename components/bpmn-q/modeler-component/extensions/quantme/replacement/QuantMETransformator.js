d7e81eec3b5fc1dfcc185a183cd479b37cfa11dc
Benjamin Weder <benjamin.weder@iaas.uni-stuttgart.de>
2023-10-31 15:21:38 +0100

    Apply linter

--- components/bpmn-q/modeler-component/extensions/quantme/replacement/QuantMETransformator.js
+++ components/bpmn-q/modeler-component/extensions/quantme/replacement/QuantMETransformator.js
@@ -54,7 +54,7 @@ export async function startQuantmeReplacementProcess(
   }
 
   // TODO: perform rewriting
-  console.log("Rewriting...")
+  console.log("Rewriting...");
 
   // get all QuantME modeling constructs from the process
   let replacementConstructs = getQuantMETasks(rootElement, elementRegistry);
