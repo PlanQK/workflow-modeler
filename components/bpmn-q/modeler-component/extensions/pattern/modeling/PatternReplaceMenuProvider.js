98fd9f6d481576990e69b8537920a1bc02105311
LaviniaStiliadou <livia_16@live.de>
2023-11-11 19:53:31 +0100

    fix replace menu

--- components/bpmn-q/modeler-component/extensions/pattern/modeling/PatternReplaceMenuProvider.js
+++ components/bpmn-q/modeler-component/extensions/pattern/modeling/PatternReplaceMenuProvider.js
@@ -10,12 +10,11 @@
  */
 
 import * as quantmeReplaceOptions from "./PatternReplaceOptions";
-import { is, isAny } from "bpmn-js/lib/util/ModelUtil";
+import { is } from "bpmn-js/lib/util/ModelUtil";
 import {
   createMenuEntries,
   createMoreOptionsEntryWithReturn,
 } from "../../../editor/util/PopupMenuUtilities";
-import * as dataConsts from "../../data-extension/Constants";
 import { filter } from "min-dash";
 import { isDifferentType } from "bpmn-js/lib/features/popup-menu/util/TypeUtil";
 import * as consts from "../Constants";
@@ -48,17 +47,7 @@ export default class PatternReplaceMenuProvider {
   getPopupMenuEntries(element) {
     const self = this;
     return function (entries) {
-      // do not show entries for extension elements of other plugins, except for DataMapObjects to list the loaded
-      // configurations for DataMapObjects
-
-      // add menu entries for the loaded configuration which can be applied to DataMapObjects
-      if (
-        isAny(element, ["bpmn:DataObjectReference", dataConsts.DATA_MAP_OBJECT])
-      ) {
-        const dataEntries = self.createPatternDataEntry(element);
-        return Object.assign(dataEntries, entries);
-      }
-
+    
       // add additional elements to replace tasks
       if (is(element, "bpmn:Event")) {
         console.log("HIER EVNET")
