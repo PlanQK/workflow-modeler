dc50696ebd8b6f0c3d2efb667a04a29140023a2e
LaviniaStiliadou <livia_16@live.de>
2023-11-16 16:00:49 +0100

    add header

--- components/bpmn-q/modeler-component/extensions/opentosca/modeling/OpenTOSCAContextPadProvider.js
+++ components/bpmn-q/modeler-component/extensions/opentosca/modeling/OpenTOSCAContextPadProvider.js
@@ -1,3 +1,13 @@
+/**
+ * Copyright (c) 2023 Institute of Architecture of Application Systems -
+ * University of Stuttgart
+ *
+ * This program and the accompanying materials are made available under the
+ * terms the Apache Software License 2.0
+ * which is available at https://www.apache.org/licenses/LICENSE-2.0.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 import inherits from "inherits";
 
 import ContextPadProvider from "bpmn-js/lib/features/context-pad/ContextPadProvider";
