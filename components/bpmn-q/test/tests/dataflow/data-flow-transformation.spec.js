5d259082311a98e61a65d36347ff66186638d2a5
Sebastian Wagner <sebastian.wagner@stoneone.de>
2023-12-07 13:46:06 +0100

    fix: data flow test cases

--- components/bpmn-q/test/tests/dataflow/data-flow-transformation.spec.js
+++ components/bpmn-q/test/tests/dataflow/data-flow-transformation.spec.js
@@ -1,31 +1,23 @@
-import {
-  getAllConfigs,
-  setPluginConfig,
-} from "../../../modeler-component/editor/plugin/PluginConfigHandler";
-import { createTempModeler } from "../../../modeler-component/editor/ModelerHandler";
-import { loadDiagram } from "../../../modeler-component/editor/util/IoUtilities";
+import {getAllConfigs, setPluginConfig,} from "../../../modeler-component/editor/plugin/PluginConfigHandler";
+import {createTempModeler} from "../../../modeler-component/editor/ModelerHandler";
+import {loadDiagram} from "../../../modeler-component/editor/util/IoUtilities";
 import {
   COMPLETE_EXAMPLE_WORKFLOW,
-  INPUT_TRANSFORMATION_ASSOCIATION,
-  PROCESS_INPUT_WORKFLOW,
-  USED_BEFORE_INIT_WORKFLOW,
-  SPLIT_MERGE_WORKFLOW,
-  ONLY_LOCAL_VARS_WORKFLOW,
-  MULTIPLE_START_EVENTS_WORKFLOW,
-  SUBPROCESS_WORKFLOW,
-  MULTI_IO_WORKFLOW,
   DOCUMENTATION_WORKFLOW,
+  INPUT_TRANSFORMATION_ASSOCIATION,
+  MULTI_IO_WORKFLOW,
+  PROCESS_INPUT_WORKFLOW,
   PURE_BPMN_WORKFLOW,
+  SPLIT_MERGE_WORKFLOW,
   UNTRANSFORMED_BPMN_WORKFLOW,
 } from "./DataFlowWorkflows";
-import { startDataFlowReplacementProcess } from "../../../modeler-component/extensions/data-extension/transformation/TransformationManager";
-import { expect } from "chai";
 import {
-  getDocumentation,
-  getRootProcess,
-} from "../../../modeler-component/editor/util/ModellingUtilities";
-import { getAllElementsForProcess } from "../../../modeler-component/editor/util/TransformationUtilities";
-import { testTaskIo } from "../helpers/PropertiesHelper";
+  startDataFlowReplacementProcess
+} from "../../../modeler-component/extensions/data-extension/transformation/TransformationManager";
+import {expect} from "chai";
+import {getDocumentation, getRootProcess,} from "../../../modeler-component/editor/util/ModellingUtilities";
+import {getAllElementsForProcess} from "../../../modeler-component/editor/util/TransformationUtilities";
+import {testTaskIo} from "../helpers/PropertiesHelper";
 
 describe("Test the TransformationManager of the data flow extension.", function () {
   describe("Test startDataFlowReplacementProcess()", function () {
@@ -63,34 +55,7 @@ describe("Test the TransformationManager of the data flow extension.", function 
       )[0].element;
       const startEventElement = elementRegistry.get(startEventBo.id);
 
-      const globalTaskElement = startEventElement.outgoing[0].target;
-
-      expect(globalTaskElement.type).to.equal("bpmn:Task");
-      expect(globalTaskElement.businessObject.name).to.equal(
-        "Create Process Variables [Generated]"
-      );
-
-      testTaskIo(
-        globalTaskElement,
-        {},
-        {
-          JSON1: {
-            json_value: '{ "name": "The JSON File"}',
-            author: "Steve Jobs",
-            creation_date: "12.04.2023",
-          },
-          JSON2: {
-            json: "${JSON1.json_value}",
-          },
-          ["XSD Repository"]: {
-            url: "http://localhost:8080/",
-            access_token: "adf1asd4fasd4f3asd54f3a4sds566s6dfa3dd",
-          },
-        },
-        bpmnFactory
-      );
-
-      const serviceTaskElement = globalTaskElement.outgoing[0].target;
+      const serviceTaskElement = startEventElement.outgoing[0].target;
 
       expect(serviceTaskElement.type).to.equal("bpmn:ServiceTask");
       expect(serviceTaskElement.businessObject.name).to.equal(
@@ -110,9 +75,7 @@ describe("Test the TransformationManager of the data flow extension.", function 
         },
         {
           test_output: "yes",
-          XML1: {
-            xml_value: "<root>entry</root>",
-          },
+          XML1: "${result}",
         },
         bpmnFactory
       );
@@ -205,32 +168,7 @@ describe("Test the TransformationManager of the data flow extension.", function 
 
       const startEventElement = elementRegistry.get("StartEvent_1");
 
-      const globalTaskElement = startEventElement.outgoing[0].target;
-
-      expect(globalTaskElement.type).to.equal("bpmn:Task");
-      expect(globalTaskElement.businessObject.name).to.equal(
-        "Create Process Variables [Generated]"
-      );
-
-      testTaskIo(
-        globalTaskElement,
-        {},
-        {
-          TransfIn1: {
-            type1: "Start",
-            type2: "End",
-          },
-          In2: {
-            author: "a",
-          },
-          In3: {
-            user: "u",
-          },
-        },
-        bpmnFactory
-      );
-
-      const task1Element = globalTaskElement.outgoing[0].target;
+      const task1Element = startEventElement.outgoing[0].target;
 
       testTaskIo(
         task1Element,
@@ -244,13 +182,8 @@ describe("Test the TransformationManager of the data flow extension.", function 
           },
         },
         {
-          Out1: {
-            all: "all",
-            failures: "error",
-          },
-          Out2: {
-            warnings: "danger",
-          },
+          Out1: "${result}",
+          Out2: "${result}"
         },
         bpmnFactory
       );
@@ -303,173 +236,6 @@ describe("Test the TransformationManager of the data flow extension.", function 
       );
       expect(getDocumentation(owner.businessObject)).to.equal(
         '\n \n This object was created through a transformation of Address. The transformation was defined by the following expressions: \n{"owner":"${Address.Owner}"}\n \n Replaced DataMapObject, represents the following data: \n{"owner":"${Address.Owner}"}'
-      );
-    });
-  });
-
-  describe("Test createGlobalProcessVariables()", function () {
-    afterEach(function () {
-      setPluginConfig([]);
-    });
-
-    it("Should not create a task for publishing global process variables", async function () {
-      setPluginConfig([{ name: "dataflow" }]);
-
-      const result = await startDataFlowReplacementProcess(
-        ONLY_LOCAL_VARS_WORKFLOW
-      );
-
-      expect(result.status).to.equal("transformed");
-
-      // load transformed workflow in modeler to check elements
-      const modeler = createTempModeler();
-      await loadDiagram(result.xml, modeler);
-
-      let elementRegistry = modeler.get("elementRegistry");
-
-      const startEventElement = elementRegistry.get("StartEvent_1");
-
-      // no task for publishing process variables created
-      expect(
-        startEventElement.outgoing[0].target.businessObject.name
-      ).to.not.equal("Create Process Variables [Generated]");
-    });
-
-    it("Should create a task for process variables because a DataMapObject is used before its initialization", async function () {
-      setPluginConfig([{ name: "dataflow" }]);
-
-      const result = await startDataFlowReplacementProcess(
-        USED_BEFORE_INIT_WORKFLOW
-      );
-
-      expect(result.status).to.equal("transformed");
-
-      // load transformed workflow in modeler to check elements
-      const modeler = createTempModeler();
-      await loadDiagram(result.xml, modeler);
-
-      let elementRegistry = modeler.get("elementRegistry");
-      let bpmnFactory = modeler.get("bpmnFactory");
-
-      const startEventElement = elementRegistry.get("StartEvent_1");
-
-      const globalTaskElement = startEventElement.outgoing[0].target;
-
-      expect(globalTaskElement.type).to.equal("bpmn:Task");
-      expect(globalTaskElement.businessObject.name).to.equal(
-        "Create Process Variables [Generated]"
-      );
-
-      testTaskIo(
-        globalTaskElement,
-        {},
-        {
-          Data: {
-            rec1: "12",
-            rec2: "21",
-          },
-        },
-        bpmnFactory
-      );
-    });
-
-    it("Should create a task for process variables for each start event", async function () {
-      setPluginConfig([{ name: "dataflow" }]);
-
-      const result = await startDataFlowReplacementProcess(
-        MULTIPLE_START_EVENTS_WORKFLOW
-      );
-
-      expect(result.status).to.equal("transformed");
-
-      // load transformed workflow in modeler to check elements
-      const modeler = createTempModeler();
-      await loadDiagram(result.xml, modeler);
-
-      let elementRegistry = modeler.get("elementRegistry");
-      let bpmnFactory = modeler.get("bpmnFactory");
-
-      const startEventTimerElement = elementRegistry.get("TimerStartEvent_1");
-      const startEvent1Element = elementRegistry.get("StartEvent_1");
-      const startEvent2Element = elementRegistry.get("StartEvent_2");
-
-      // there should exist a new task for each start event
-      for (let startEventElement of [
-        startEventTimerElement,
-        startEvent1Element,
-        startEvent2Element,
-      ]) {
-        let globalTaskElement = startEventElement.outgoing[0].target;
-
-        expect(globalTaskElement.type).to.equal("bpmn:Task");
-        expect(globalTaskElement.businessObject.name).to.equal(
-          "Create Process Variables [Generated]"
-        );
-
-        testTaskIo(
-          globalTaskElement,
-          {},
-          {
-            Data1: {
-              a: "1",
-              b: "2",
-              c: "3",
-            },
-            Data2: {
-              number: "3,42157694251",
-            },
-            Repo1: {
-              url: "google.com",
-              username: "user",
-            },
-          },
-          bpmnFactory
-        );
-      }
-    });
-
-    it("Should create a task for process variables inside the subprocess", async function () {
-      setPluginConfig([{ name: "dataflow" }]);
-
-      const result = await startDataFlowReplacementProcess(SUBPROCESS_WORKFLOW);
-
-      expect(result.status).to.equal("transformed");
-
-      // load transformed workflow in modeler to check elements
-      const modeler = createTempModeler();
-      await loadDiagram(result.xml, modeler);
-
-      let elementRegistry = modeler.get("elementRegistry");
-      let bpmnFactory = modeler.get("bpmnFactory");
-
-      const startEventElement = elementRegistry.get("Start1");
-
-      // there should be no task for process variables after the start event
-      const taskAfterStart = startEventElement.outgoing[0].target;
-      expect(taskAfterStart.businessObject.name).to.not.equal(
-        "Create Process Variables [Generated]"
-      );
-      expect(taskAfterStart.businessObject.name).to.equal("Task1");
-
-      // there should be a bew task in the subprocess
-      const subStartEventElement = elementRegistry.get("SubStart1");
-
-      let globalTaskElement = subStartEventElement.outgoing[0].target;
-
-      expect(globalTaskElement.type).to.equal("bpmn:Task");
-      expect(globalTaskElement.businessObject.name).to.equal(
-        "Create Process Variables [Generated]"
-      );
-
-      testTaskIo(
-        globalTaskElement,
-        {},
-        {
-          SubData1: {
-            sub1: "1",
-          },
-        },
-        bpmnFactory
       );
     });
   });
@@ -606,27 +372,6 @@ describe("Test the TransformationManager of the data flow extension.", function 
 
       let elementRegistry = modeler.get("elementRegistry");
       let bpmnFactory = modeler.get("bpmnFactory");
-
-      const startEventElement = elementRegistry.get("Start1");
-
-      const globalTaskElement = startEventElement.outgoing[0].target;
-
-      expect(globalTaskElement.type).to.equal("bpmn:Task");
-      expect(globalTaskElement.businessObject.name).to.equal(
-        "Create Process Variables [Generated]"
-      );
-
-      testTaskIo(
-        globalTaskElement,
-        {},
-        {
-          Data1: {
-            a: "1",
-            b: "2",
-          },
-        },
-        bpmnFactory
-      );
 
       const task1Element = elementRegistry.get("Task1");
 
