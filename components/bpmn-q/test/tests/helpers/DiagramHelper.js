f6cba05e219126f87fa90c5378196d595ff0ccab
Sebastian Wagner <sebastian.wagner@stoneone.de>
2023-12-19 12:34:28 +0100

    PlanQK test workflow updated

--- components/bpmn-q/test/tests/helpers/DiagramHelper.js
+++ components/bpmn-q/test/tests/helpers/DiagramHelper.js
@@ -1,6 +1,6 @@
 const validPlanqkDiagram = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
   "<bpmn2:definitions xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:bpmn2=\"http://www.omg.org/spec/BPMN/20100524/MODEL\" xmlns:bpmndi=\"http://www.omg.org/spec/BPMN/20100524/DI\" xmlns:dc=\"http://www.omg.org/spec/DD/20100524/DC\" xmlns:planqk=\"https://platform.planqk.de\" xmlns:dataflow=\"https://github.com/data/transformation\" xmlns:di=\"http://www.omg.org/spec/DD/20100524/DI\" id=\"sample-diagram\" targetNamespace=\"http://bpmn.io/schema/bpmn\" xsi:schemaLocation=\"http://www.omg.org/spec/BPMN/20100524/MODEL BPMN20.xsd\">\n" +
-  "  <bpmn2:process id=\"Process_1\" isExecutable=\"true\">\n" +
+  "  <bpmn2:process name=\"Test Process\" id=\"Process_1\" isExecutable=\"true\">\n" +
   "    <bpmn2:startEvent id=\"StartEvent_1\">\n" +
   "      <bpmn2:outgoing>Flow_0fp0far</bpmn2:outgoing>\n" +
   "    </bpmn2:startEvent>\n" +
