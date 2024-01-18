8d5e63532c9ef570a71577de9580d4321db4346e
LaviniaStiliadou <livia_16@live.de>
2024-01-18 15:08:14 +0100

    delete gate error mitigation pattern during transformation

--- components/bpmn-q/modeler-component/extensions/pattern/replacement/mitigation/MitigationPatternHandler.js
+++ components/bpmn-q/modeler-component/extensions/pattern/replacement/mitigation/MitigationPatternHandler.js
@@ -20,35 +20,39 @@ export async function replaceMitigationPattern(
 ) {
   console.log(
     "Replace mitigation pattern " +
-      mitigationPattern.id +
-      "of parent " +
-      parent.id
+    mitigationPattern.id +
+    "of parent " +
+    parent.id
   );
   let modeling = modeler.get("modeling");
   let elementRegistry = modeler.get("elementRegistry");
   let host = elementRegistry.get(mitigationPattern.id).host;
   let type = quantmeConsts.READOUT_ERROR_MITIGATION_TASK;
-  if (mitigationPattern.type === consts.READOUT_ERROR_MITIGATION) {
-    type = quantmeConsts.READOUT_ERROR_MITIGATION_TASK;
-  }
 
-  let internHost = elementRegistry.get(host.id);
-  let mitigationTask = modeling.createShape(
-    { type: type },
-    { x: 50, y: 50 },
-    parent,
-    {}
-  );
-  let startEventBo = elementRegistry.get(mitigationTask.id).businessObject;
-  startEventBo.name = "Mitigate Errors";
-  let outgoingFlows = [];
-  host.outgoing.forEach((element) => {
-    outgoingFlows.push(elementRegistry.get(element.id));
-    modeling.connect(mitigationTask, elementRegistry.get(element.target.id), {
-      type: "bpmn:SequenceFlow",
+  // currently no replacement for gate error mitigation pattern
+  if (mitigationPattern.$type === consts.GATE_ERROR_MITIGATION) {
+    type = quantmeConsts.READOUT_ERROR_MITIGATION_TASK;
+    const pattern = elementRegistry.get(mitigationPattern.id);
+    return { replaced: true, flows: [], pattern: pattern };
+  } else {
+    let internHost = elementRegistry.get(host.id);
+    let mitigationTask = modeling.createShape(
+      { type: quantmeConsts.READOUT_ERROR_MITIGATION_TASK},
+      { x: 50, y: 50 },
+      parent,
+      {}
+    );
+    let readoutMitigationTaskBo = elementRegistry.get(mitigationTask.id).businessObject;
+    readoutMitigationTaskBo.name = "Mitigate Errors";
+    let outgoingFlows = [];
+    host.outgoing.forEach((element) => {
+      outgoingFlows.push(elementRegistry.get(element.id));
+      modeling.connect(mitigationTask, elementRegistry.get(element.target.id), {
+        type: "bpmn:SequenceFlow",
+      });
     });
-  });
-  modeling.connect(internHost, mitigationTask, { type: "bpmn:SequenceFlow" });
-  const pattern = elementRegistry.get(mitigationPattern.id);
-  return { replaced: true, flows: outgoingFlows, pattern: pattern };
+    modeling.connect(internHost, mitigationTask, { type: "bpmn:SequenceFlow" });
+    const pattern = elementRegistry.get(mitigationPattern.id);
+    return { replaced: true, flows: outgoingFlows, pattern: pattern };
+  }
 }
