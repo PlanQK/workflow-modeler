36e78cf112bf7e184e19c6e2ccb2923a1e018485
mbeisel <beiselmn@gmail.com>
2024-04-09 12:11:48 +0200

    fix selfserviceapplicationurl spelling disable view removal on change attempt policy adaptation for subprocesses

--- components/bpmn-q/modeler-component/editor/modeling/ModelerRules.js
+++ components/bpmn-q/modeler-component/editor/modeling/ModelerRules.js
@@ -58,7 +58,7 @@ export default class ModelerRules extends BpmnRules {
         });
 
         // clear views since they become invalid if changes are applied
-        modeler.views = [];
+        // modeler.views = [];
       }
     });
   }
